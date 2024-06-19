79b30a5d56fa6dd03579944a5e78074b3dda411a
BurnaevaAnna <burnaeva.annet@gmail.com>
2024-06-19 16:21:55 +0300

    added test for login with lock_out_user

--- src/test/java/pages/AuthPage.java
+++ src/test/java/pages/AuthPage.java
@@ -7,4 +7,5 @@ public class AuthPage {
     public SelenideElement userNameInput = Selenide.$x("//*[@id='user-name']");
     public SelenideElement passwordInput = Selenide.$x("//*[@id='password']");
     public SelenideElement loginButton = Selenide.$x("//*[@id='login-button']");
+    public SelenideElement dataTestError  = Selenide.$x("//*[@data-test='error']");
 }
