9a3cb6fa4942371c6bbb49721b79f9dbcc8f8961
AnnaRubina <anna_mihailova@bk.ru>
2024-06-24 11:35:54 +0300

    Добавлен тест на добавление рюкзака в корзину

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -5,4 +5,6 @@ import com.codeborne.selenide.SelenideElement;
 
 public class MainPage {
     public SelenideElement title = Selenide.$x("//span[@class='title']");
+    public SelenideElement shopping_cart_link = Selenide.$x("//*[@data-test='shopping-cart-badge']");
+    public SelenideElement add_to_cart_backpack = Selenide.$x("//*[@data-test='add-to-cart-sauce-labs-backpack']");
 }
